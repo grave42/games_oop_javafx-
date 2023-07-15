f86d41869f2bc7036b8ba839629c10a613cede20
filipp.kucherov <filipp.kucerov@r1.team>
2023-07-15 11:56:43 +0700

    Реализация Logic 3

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.black.BishopBlack;
+import ru.job4j.chess.firuges.black.PawnBlack;
+import ru.job4j.chess.firuges.white.BishopWhite;
+import ru.job4j.chess.firuges.white.RookWhite;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -20,25 +23,26 @@ public class LogicTest {
         assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
     }
 
-    @Disabled
     @Test
     public void whenMoveThenOccupiedCellException()
             throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C8));
+        logic.add(new PawnBlack(Cell.B7));
         OccupiedCellException exception = assertThrows(OccupiedCellException.class, () -> {
-            logic.move(Cell.A1, Cell.A2);
+            logic.move(Cell.C8, Cell.B7);
         });
         assertThat(exception.getMessage()).isEqualTo("Cell is not empty");
     }
 
-    @Disabled
     @Test
     public void whenMoveImpossibleMoveException()
             throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C8));
         ImpossibleMoveException exception = assertThrows(ImpossibleMoveException.class, () -> {
             logic.move(Cell.C8, Cell.C6);
         });
-        assertThat(exception.getMessage()).isEqualTo("Could not move by diagonal from С8 to С6");
+        assertThat(exception.getMessage()).isEqualTo("Could not move by diagonal from C8 to C6");
     }
 }
